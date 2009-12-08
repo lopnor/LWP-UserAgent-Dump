e75ae834b138d78de4e057a84961d96d179cd7dc
Nobuo Danjou <nobuo.danjou@gmail.com>
2009-12-08 22:29:56 +0900

    impl

--- lib/LWP/UserAgent/Dump.pm
+++ lib/LWP/UserAgent/Dump.pm
@@ -2,28 +2,49 @@ package LWP::UserAgent::Dump;
 
 use strict;
 use warnings;
+use LWP::UserAgent;
+use Data::Dumper;
 our $VERSION = '0.01';
+
+my $orig = \&LWP::UserAgent::request;
+
+sub _request {
+    my ($self, $req, @args) = @_;
+    my $res = $orig->($self, $req, @args);
+    warn $req->as_string;
+    warn $res->as_string;
+    warn Dumper $res;
+    return $res;
+}
+
+{
+    no strict 'refs';
+    no warnings 'redefine';
+    *LWP::UserAgent::request = \&_request;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-LWP::UserAgent::Dump -
+LWP::UserAgent::Dump - Dumps LWP::UserAgent requests and responses
 
 =head1 SYNOPSIS
 
-  use LWP::UserAgent::Dump;
+  % perl -I/path/to/LWP-UserAgent-Dump/lib -MLWP::UserAgent::Dump yourscript.pl
 
 =head1 DESCRIPTION
 
-LWP::UserAgent::Dump is
+LWP::UserAgent::Dump Dumps LWP::UserAgent requests and responses.
 
 =head1 AUTHOR
 
 Nobuo Danjou E<lt>nobuo.danjou@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<LWP::UserAgent>
 
 =head1 LICENSE
 
